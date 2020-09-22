06479c305657a5e6a8135deee643f86f1a0c89e6
yinxulai <yinxulai@hotmail.com>
2020-09-22 15:01:44 +0800

    修复生成上传 Token 针对 encodedPutPolicy 使用 urlSafe 编码的问题

--- lib/src/auth/auth.dart
+++ lib/src/auth/auth.dart
@@ -46,7 +46,7 @@ class Auth {
     assert(putPolicy != null);
 
     var data = jsonEncode(putPolicy);
-    var encodedPutPolicy = base64Encode(utf8.encode(data));
+    var encodedPutPolicy = base64Url.encode(utf8.encode(data));
     var baseToken = generateAccessToken(bytes: utf8.encode(encodedPutPolicy));
     return '$baseToken:$encodedPutPolicy';
   }
@@ -77,7 +77,7 @@ class Auth {
     var hmacEncoder = Hmac(sha1, utf8.encode(secretKey));
 
     var sign = hmacEncoder.convert(bytes);
-    var encodedSign = Base64Encoder.urlSafe().convert(sign.bytes);
+    var encodedSign = base64Url.encode(sign.bytes);
     return '$accessKey:$encodedSign';
   }
 
@@ -98,14 +98,14 @@ class Auth {
     ///
     /// https://github.com/qbox/product/blob/master/kodo/auths/UpToken.md#admin-uptoken-authorization
     if (segments.length >= 3) {
-      if (segments.last == null || segments.last == '') {
+      if (segments.last == '') {
         throw ArgumentError('invalid token');
       }
 
       putPolicy = PutPolicy.fromJson(
         jsonDecode(
           String.fromCharCodes(
-            base64.decode(
+            base64Url.decode(
               segments.last,
             ),
           ),
