03b14bdc4b0ecec8c5898689a8b530bb9e9182ad
黄斌杰 <akira.binjie@gmail.com>
2020-10-15 19:10:18 +0800

    没有敏感数据不生成 token

--- test/config.dart
+++ test/config.dart
@@ -17,25 +17,21 @@ bool get isSensitiveDataDefined {
 
 void configEnv() {
   setUpAll(() {
-    load();
-    if (!isEveryDefined([
-      'QINIU_DART_SDK_ACCESS_KEY',
-      'QINIU_DART_SDK_SECRET_KEY',
-      'QINIU_DART_SDK_TOKEN_SCOPE'
-    ])) {
+    if (isSensitiveDataDefined) {
+      var auth = Auth(
+        accessKey: env['QINIU_DART_SDK_ACCESS_KEY'],
+        secretKey: env['QINIU_DART_SDK_SECRET_KEY'],
+      );
+
+      token = auth.generateUploadToken(
+        putPolicy: PutPolicy(
+            insertOnly: 0,
+            scope: env['QINIU_DART_SDK_TOKEN_SCOPE'],
+            deadline: DateTime.now().millisecondsSinceEpoch + 3600),
+      );
+    } else {
       stderr.writeln('没有在 .env 文件里配置测试用的必要信息，一些测试用例会被跳过');
     }
-    var auth = Auth(
-      accessKey: env['QINIU_DART_SDK_ACCESS_KEY'],
-      secretKey: env['QINIU_DART_SDK_SECRET_KEY'],
-    );
-
-    token = auth.generateUploadToken(
-      putPolicy: PutPolicy(
-          insertOnly: 0,
-          scope: env['QINIU_DART_SDK_TOKEN_SCOPE'],
-          deadline: DateTime.now().millisecondsSinceEpoch + 3600),
-    );
   });
 
   tearDownAll(clean);
