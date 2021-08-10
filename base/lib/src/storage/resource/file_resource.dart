ecdc21bfbe8a640aba4169bef2db713fb40b5b8e
黄斌杰 <akira.binjie@gmail.com>
2021-08-10 20:38:46 +0800

    删除注释

--- base/lib/src/storage/resource/file_resource.dart
+++ base/lib/src/storage/resource/file_resource.dart
@@ -10,10 +10,6 @@ class FileResource extends Resource {
 
   @override
   void open() {
-    // 子任务 UploadPartTask 从 file 去 open 的话虽然上传精度会颗粒更细但是会导致可能读不出文件的问题
-    // 可能 close 没办法立即关闭 file stream，而延迟 close 了，导致某次 open 的 stream 被立即关闭
-    // 所以读不出内容了
-    // 改成这里 open 一次，子任务从中读取 bytes
     raf = file.openSync();
   }
 
