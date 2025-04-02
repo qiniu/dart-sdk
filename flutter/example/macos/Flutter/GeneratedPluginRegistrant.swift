0032f8b049673f9ce92f15683c960bfea8ae8b54
zzq <i@zhangzqs.cn>
2025-04-02 14:28:43 +0800

    更新macos平台文件

--- flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
+import file_picker
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
 }
