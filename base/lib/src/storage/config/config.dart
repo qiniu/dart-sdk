a3ea19e30ab6cc71a93cac8ff1f8fd3efd56a877
YangSen-qn <yangsen@qiniu.com>
2026-01-08 15:53:38 +0800

    fix: put one file, during first req, if all hosts are frozen, randomly unfreeze one for use

--- base/lib/src/storage/config/config.dart
+++ base/lib/src/storage/config/config.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:dio/dio.dart';
 import 'package:crypto/crypto.dart';
--- base/lib/src/storage/task/request_task_manager.dart
+++ base/lib/src/storage/task/request_task_manager.dart
@@ -1,11 +1,82 @@
 part of 'request_task.dart';
 
+/// HostProvider 的包装类，提供首次请求失败时的重试逻辑
+///
+/// 当第一次获取上传域名时，如果所有域名都被冻结导致失败，
+/// 此包装器会自动解冻一个域名并重试一次。
+class _HostProvider extends HostProvider {
+  final HostProvider _hostprovider;
+
+  bool _hasGetUpHost = false;
+
+  _HostProvider(this._hostprovider);
+
+  @override
+  Future<String> getUpHost({
+    required String accessKey,
+    required String bucket,
+    bool accelerateUploading = false,
+    bool transregional = false,
+    int regionIndex = 0,
+  }) async {
+    var retryCount = 0;
+    while (true) {
+      try {
+        final host = await _hostprovider.getUpHost(
+          accessKey: accessKey,
+          bucket: bucket,
+          accelerateUploading: accelerateUploading,
+          transregional: transregional,
+          regionIndex: regionIndex,
+        );
+        _hasGetUpHost = true;
+        return host;
+      } on StorageError catch (error) {
+        if (_hasGetUpHost) {
+          rethrow;
+        }
+        if (error.type != StorageErrorType.NO_AVAILABLE_HOST) {
+          rethrow;
+        }
+        if (retryCount >= 3) {
+          rethrow;
+        }
+        // 如果第一次获取上传域名就失败，尝试解冻一个上传域名后重试一次
+        _hostprovider.unfreezeOne();
+        retryCount++;
+      }
+    }
+  }
+
+  @override
+  void freezeHost(String host) {
+    _hostprovider.freezeHost(host);
+  }
+
+  @override
+  bool isFrozen(String host) {
+    return _hostprovider.isFrozen(host);
+  }
+
+  @override
+  void unfreezeOne() {
+    _hostprovider.unfreezeOne();
+  }
+}
+
 class RequestTaskManager extends TaskManager {
-  final Config config;
+  late final Config config;
 
   RequestTaskManager({
-    required this.config,
-  });
+    required Config config,
+  }) {
+    this.config = Config(
+      hostProvider: _HostProvider(config.hostProvider),
+      cacheProvider: config.cacheProvider,
+      httpClientAdapter: config.httpClientAdapter,
+      retryLimit: config.retryLimit,
+    );
+  }
 
   @override
   void addTask(covariant RequestTask task) {
